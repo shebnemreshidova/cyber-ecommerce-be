5ca76c4b4fb05ef0ecb2a7462ca7f31905903279
shebnemreshidova <rshebnem01@gmail.com>
2026-01-31 20:27:46 +0400

    test

--- src/db/Database.ts
+++ src/db/Database.ts
@@ -1,47 +1,53 @@
 import { MongoClient, Db } from "mongodb";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 let instance: Database | null = null;
 
 class Database {
-    private client: MongoClient | null = null;
-    private db: Db | null = null;
+  private client: MongoClient | null = null;
+  private db: Db | null = null;
 
-    constructor() {
-        if (!instance) {
-            instance = this;
-        }
-        return instance;
+  constructor() {
+    if (!instance) {
+      instance = this;
     }
+    return instance;
+  }
 
-    async connect(uri: string, dbName: string) {
-        try {
-            if (!uri) throw new Error('MongoDB URI is not provided');
-            
-            this.client = new MongoClient(uri);
-            await this.client.connect();
-            
-            this.db = this.client.db(dbName);
-            
-            console.log('MongoDB connected successfully');
-        } catch (error: any) {
-            console.error('MongoDB connection failed:', error.message);
-            process.exit(1);
-        }
-    }
+  async connect(uri: string, dbName: string) {
+    try {
+      if (!uri) throw new Error('MongoDB URI is not provided');
 
-    getDb(): Db {
-        if (!this.db) {
-            throw new Error('Database not connected');
-        }
-        return this.db;
-    }
+      this.client = new MongoClient(uri, {
+        ssl: uri.startsWith("mongodb+srv://"), // Atlas üçün SSL
+        tlsAllowInvalidCertificates: uri.startsWith("mongodb+srv://"),
+      });
 
-    async disconnect() {
-        if (this.client) {
-            await this.client.close();
-            console.log('MongoDB disconnected');
-        }
+      await this.client.connect();
+      this.db = this.client.db(dbName);
+
+      console.log('MongoDB connected successfully');
+    } catch (error: any) {
+      console.error('MongoDB connection failed:', error.message);
+      process.exit(1);
     }
+  }
+
+  getDb(): Db {
+    if (!this.db) {
+      throw new Error('Database not connected');
+    }
+    return this.db;
+  }
+
+  async disconnect() {
+    if (this.client) {
+      await this.client.close();
+      console.log('MongoDB disconnected');
+    }
+  }
 }
 
 export default new Database();
