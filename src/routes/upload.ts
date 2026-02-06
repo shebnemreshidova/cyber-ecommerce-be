7c8ba679a5104f25af1c9f58a6bdf280ef0a1c5e
shebnemreshidova <rshebnem01@gmail.com>
2026-02-06 19:12:28 +0400

    test deploy

--- src/routes/upload.ts
+++ src/routes/upload.ts
@@ -2,11 +2,9 @@ import multer from "multer";
 import { CloudinaryStorage } from "multer-storage-cloudinary";
 import cloudinary from "../config/cloudinary";
 
-const isProduction = process.env.NODE_ENV === "production";
 
 let storage;
 
-if (isProduction) {
   storage = new CloudinaryStorage({
     cloudinary,
     params: async () => ({
@@ -14,16 +12,6 @@ if (isProduction) {
       allowed_formats: ["jpg", "png", "jpeg", "webp"],
     }),
   });
-} else {
-  storage = multer.diskStorage({
-    destination: "uploads/",
-    
-    filename: (req, file, cb) => {
-      const uniqueName = Date.now() + "-" + file.originalname;
-      cb(null, uniqueName);
-    },
-  });
-}
 
 const upload = multer({ storage });
 
